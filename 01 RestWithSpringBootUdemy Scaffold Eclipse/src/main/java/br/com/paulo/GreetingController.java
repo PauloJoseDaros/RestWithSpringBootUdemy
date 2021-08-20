767e361a33cb83d34a5e85089f5623db1896f7da
paulojosedaros <paulodaros@gmail.com>
2021-08-20 07:41:45 -0300

    ola mudao

---
+++ 01 RestWithSpringBootUdemy Scaffold Eclipse/src/main/java/br/com/paulo/GreetingController.java
@@ -0,0 +1,20 @@
+package br.com.paulo;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+	
+	private static final String template = "Ol�, %s!";
+	private final AtomicLong counter = new AtomicLong();
+	
+	@RequestMapping("/greeting")
+	public Greeting greeting(@RequestParam(value="name", defaultValue = "Mund�o") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+	
+}
