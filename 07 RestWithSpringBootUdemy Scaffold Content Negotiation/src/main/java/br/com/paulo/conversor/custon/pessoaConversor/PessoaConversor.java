ce8c9f2a5a90838e47fc6adeedbe0a231a3d9cc8
paulojosedaros <paulodaros@gmail.com>
2021-09-03 08:26:10 -0300

    inicio content negotiation

---
+++ 07 RestWithSpringBootUdemy Scaffold Content Negotiation/src/main/java/br/com/paulo/conversor/custon/pessoaConversor/PessoaConversor.java
@@ -0,0 +1,34 @@
+package br.com.paulo.conversor.custon.pessoaConversor;
+
+import java.util.Date;
+
+import br.com.paulo.data.model.Pessoa;
+
+public class PessoaConversor {
+	
+//	public PessoaVOV2 converterEntidadeParaVO(Pessoa pessoa) {
+//		
+//		PessoaVOV2 vo = new PessoaVOV2();
+//		
+//		vo.setIdPessoa(pessoa.getIdPessoa());
+//		vo.setEndereco(pessoa.getEndereco());
+//		vo.setGenero(pessoa.getGenero());
+//		vo.setNome(pessoa.getNome());
+//		vo.setDataNascimento(new Date());
+//		vo.setSobrenome(pessoa.getSobrenome());
+//		return vo;
+//	}
+//	
+//	public Pessoa converterVOparaEntidade(PessoaVOV2 pessoaVOV2) {
+//		
+//		Pessoa pessoa = new Pessoa();
+//		
+//		pessoa.setIdPessoa(pessoaVOV2.getIdPessoa());
+//		pessoa.setEndereco(pessoaVOV2.getEndereco());
+//		pessoa.setGenero(pessoaVOV2.getGenero());
+//		pessoa.setNome(pessoaVOV2.getNome());
+//		pessoa.setSobrenome(pessoaVOV2.getSobrenome());
+//		return pessoa;
+//	}
+
+}
