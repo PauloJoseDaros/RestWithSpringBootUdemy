35f807170446a6caf13fcb40b565cfc2d2dca28d
paulojosedaros <paulodaros@gmail.com>
2021-09-06 08:39:44 -0300

    INICIO IMPLEMENTANDO HATEOAS

---
+++ 08 RestWithSpringBootUdemy Scaffold HATEOAS/src/main/java/br/com/paulo/control/PessoaController.java
@@ -0,0 +1,58 @@
+package br.com.paulo.control;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.paulo.data.vo.v1.PessoaVO;
+import br.com.paulo.servicos.PessoaService;
+
+
+@RestController
+@RequestMapping("/api/pessoa/v1")
+public class PessoaController {
+	
+	@Autowired
+	private PessoaService peServices;
+	
+	@GetMapping(produces = {"application/json","application/xml","application/x-yaml"})
+	public List<PessoaVO> pesquisaTodos()  {
+		return peServices.pesquisaTodos();
+	}
+	
+	@GetMapping(value = "/{id}", produces = {"application/json","application/xml","application/x-yaml"})
+	public PessoaVO pesquisaPorId(@PathVariable("id") Long id)  {
+		
+		return peServices.pesquisaPorID(id);
+	}
+	
+	@PostMapping(produces = {"application/json","application/xml","application/x-yaml"},
+			consumes = {"application/json","application/xml","application/x-yaml"})
+	public PessoaVO salvar(@RequestBody PessoaVO pPessoaVO)  {
+		
+		return peServices.salvar(pPessoaVO);
+	}
+	
+	@PutMapping(produces = {"application/json","application/xml","application/x-yaml"},
+			consumes = {"application/json","application/xml","application/x-yaml"})
+	public PessoaVO alterar(@RequestBody PessoaVO pPessoaVO)  {
+		
+		return peServices.alterar(pPessoaVO);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<PessoaVO> deletar(@PathVariable("id") Long id)  {
+		
+		 peServices.deletar(id);
+		 return ResponseEntity.ok().build();
+	}
+}
---
+++ 08 RestWithSpringBootUdemy Scaffold HATEOAS/src/main/java/br/com/paulo/data/vo/v1/PessoaVO.java
@@ -0,0 +1,103 @@
+package br.com.paulo.data.vo.v1;
+
+import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({"idPessoa","genero","sobrenome","nome","endereco"})
+public class PessoaVO implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	private Long idPessoa;
+	@JsonProperty("primeiro_nome")
+	private String nome;
+	@JsonProperty("ultimo_nome")
+	private String sobrenome;
+	private String endereco;
+	//@JsonIgnore CUIDADO PARA SALVAR QUANDO CAMPO FOR OBRIGATORIO
+	private String genero;
+	public PessoaVO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Long getIdPessoa() {
+		return idPessoa;
+	}
+	public void setIdPessoa(Long idPessoa) {
+		this.idPessoa = idPessoa;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getGenero() {
+		return genero;
+	}
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((endereco == null) ? 0 : endereco.hashCode());
+		result = prime * result + ((genero == null) ? 0 : genero.hashCode());
+		result = prime * result + ((idPessoa == null) ? 0 : idPessoa.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((sobrenome == null) ? 0 : sobrenome.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PessoaVO other = (PessoaVO) obj;
+		if (endereco == null) {
+			if (other.endereco != null)
+				return false;
+		} else if (!endereco.equals(other.endereco))
+			return false;
+		if (genero == null) {
+			if (other.genero != null)
+				return false;
+		} else if (!genero.equals(other.genero))
+			return false;
+		if (idPessoa == null) {
+			if (other.idPessoa != null)
+				return false;
+		} else if (!idPessoa.equals(other.idPessoa))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (sobrenome == null) {
+			if (other.sobrenome != null)
+				return false;
+		} else if (!sobrenome.equals(other.sobrenome))
+			return false;
+		return true;
+	}
+	
+}
